b24953f58401abe2d257438c294107bf9c22bf3f
Mikolaj Mazurek <mikolajmazurek@gmail.com>
2019-03-24 20:28:16 +0100

    working 2

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from src.ksType import ksType
 from src.utils import file_reader
 from scipy.stats import ks_2samp
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score
 import random
+import pprint
 import numpy as np
 
 def createTeachingAndTestSets(patients):
@@ -23,6 +29,18 @@ def createTeachingAndTestSets(patients):
             testSet.append(patients[i])
 
     return teachingSet, testSet
+
+def prepareDataSet(dataSet, features=[]):
+    finalSet = []
+    finalLabelSet = []
+
+    for data in dataSet:
+        featureSet = []
+        finalLabelSet.append(data.getCancerType())
+        for x in features:
+            featureSet.append(data.getInputValues()[x])
+        finalSet.append(featureSet)
+    return finalSet, finalLabelSet
 
 def main():
     file_name = "../resources/wdbc.data"
@@ -49,7 +67,42 @@ def main():
 
     ksData.sort(key=lambda val : val.geStatistic())
 
-    for asdf in ksData:
-        print("{} ({}, {})".format(asdf.getParamID(), asdf.geStatistic(), asdf.getPValue()))
+    
+    # for asdf in ksData:
+    #     #     print("{} ({}, {})".format(asdf.getParamID(), asdf.geStatistic(), asdf.getPValue()))
+
+    # nmAlg(ksData[0], teachingSet, testSet, dataSet)
+
+    # iris = datasets.load_iris()
+    # x = iris.data
+    # y = iris.target
+    #
+    # print(x)
+    # print(y)
+
+    trainDataFeatures, trainDataLabelFeatures = prepareDataSet(teachingSet, [9, 8, 4, 0])
+    # pprint.pprint(trainDataFeatures)
+    # pprint.pprint(trainDataLabelFeatures)
+
+    testDataFeatures, testDataLabelFeatures = prepareDataSet(testSet, [9, 8, 4, 0])
+    # pprint.pprint(testDataFeatures)
+    # pprint.pprint(testDataLabelFeatures)
+
+#    testDataLabelFeatures.astype(int)
+ #   trainDataLabelFeatures.astype(int)
+
+    classifier = neighbors.KNeighborsClassifier()
+    classifier.fit(trainDataFeatures, trainDataLabelFeatures)
+    predictions = classifier.predict(testDataFeatures)
+    # print(accuracy_score(testDataLabelFeatures, predictions))
+
+    # classifier.fit([ [teachingSet[9].getInputValues()[ksData[0].getParamID()]], [teachingSet[10].getInputValues()[ksData[0].getParamID()]]],
+    #                [[0], [1]])
+    # predictions = classifier.predict(testSet[0].getInputValues()[9])
+    #
+    # print(accuracy_score(testSet[0].getCancerType(), predictions))
+# def nmAlg(param, teachingSet, trainSet, dataSet):
+
 
 main()
+
